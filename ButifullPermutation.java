1778892f4a1d53a572b318f7895c3c640985feec
nishant393 <nishantpawar393@gmail.com>
2026-01-18 09:57:56 +0530

    Butiffull permutation solved

---
+++ ButifullPermutation.java
@@ -0,0 +1,22 @@
+public class ButifullPermutation {
+
+// void evenInsert (int n ){
+//     for()
+// }
+   public static void main (String[] args){
+      int n =10
+      ;
+
+for (int i = n ; i>=1 ; i--){
+        if(i%2==0){
+            System.out.print(i + " ");
+        }
+      }
+      for (int i = n ; i>=1 ; i--){
+        if(i%2!=0){
+            System.out.print(i + " ");
+        }
+      }
+      
+   }
+}
